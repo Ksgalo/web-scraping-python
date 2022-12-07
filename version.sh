edf1f399aa667e337f2faa811844abbcb1023d34
Kevin Galindo <53983977+Ksgalo@users.noreply.github.com>
2022-12-07 12:31:44 -0500

    Update version.sh

--- version.sh
+++ version.sh
@@ -3,7 +3,7 @@
 DOMINIOPROD=kev2110/pythonprod:v1.0
 DOMINIOQA=kev2110/pybinver:v1.0
 VOLUMEN=v1:/var/
-RUTA=/var/lib/docker/volumes/v1/_data/
+RUTA=$(find / -name _data)
 
 docker volume create v1
 
